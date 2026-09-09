f23604070592436969a530c169d35a1ea3e3631c
menna <abcda876543@gmail.com>
2026-09-09 03:40:53 +0300

    Question 2

--- Program.cs
+++ Program.cs
@@ -9,8 +9,12 @@
             string upperTitle = "title.ToUpper()";
             Console.WriteLine(title);
             Console.WriteLine(upperTitle);
-            #endregion 
-
+            #endregion
+            #region Question 2 
+            string text1 = "clean code";
+            string text2 = "clean code";
+            Console.WriteLine(ReferenceEquals(text1, text2));
+            #endregion
 
         }
     }
