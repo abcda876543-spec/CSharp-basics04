22babcec69fccdb2efd8d2a153e414669f34df9d
menna <abcda876543@gmail.com>
2026-09-09 03:27:47 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace C__Basics04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Question 1
+            string title = "clean code";
+            string upperTitle = "title.ToUpper()";
+            Console.WriteLine(title);
+            Console.WriteLine(upperTitle);
+            #endregion 
+
+
+        }
+    }
+}
